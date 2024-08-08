c47217f83a70c996722550837be4e7890c2a1b79
Vishnu Nagaraj <nagarajvishnu18@gmail.com>
2024-08-08 04:09:53 -0400

    first commit

---
+++ bag.js
@@ -0,0 +1,14 @@
+const images = [
+    'images/saree.jpg',
+    'images/saree2.jpg',
+    'images/men.jpg'
+];
+
+let currentIndex = 0;
+
+function changeImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    document.getElementById('rotatingImage').src = images[currentIndex];
+}
+
+setInterval(changeImage, 3000); // Change image every 3 seconds
